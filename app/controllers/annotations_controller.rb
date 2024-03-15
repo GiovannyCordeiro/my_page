699ebcfbb589f471605e4b2d70f80d9635c05d9d
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-03-15 14:58:57 -0300

    feat: annotation controller

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -1,12 +1,28 @@
 class AnnotationsController < ApplicationController
 
+  def index
+    @annotations = Annotation.all
+  end
+
   def new
     @annotation = Annotation.new
+    @tags = Tag.all
   end
 
   def create
-    @tags = params[:tags]
+    @annotation = Annotation.new(annotation_params)
+    if @annotation.save
+      redirect_to root_path
+    else
+      render :new
+    end
     logger.info "Mensagem informativa #{params[:tags]}"
+  end
+
+  private
+
+  def annotation_params
+    params.require(:annotation).permit(:title, :description, :content)
   end
 
 end
