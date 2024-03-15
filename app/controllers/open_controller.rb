dc9386e94a1e8eec997448a27cbb88010b4ba95a
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-03-15 14:59:16 -0300

    refact: annotation dinamicly

--- app/controllers/open_controller.rb
+++ app/controllers/open_controller.rb
@@ -1,7 +1,13 @@
 # Controller that show content for public
 class OpenController < ApplicationController
-  def index; end
-  def myAnotation; end
-  def anotationTags; end
+  def index;
+    @annotations = Annotation.all
+  end
+  def myAnotation
+    @annotations = Annotation.all
+  end
+  def anotationTags
+    @annotations = Annotation.all
+  end
 end
 []
