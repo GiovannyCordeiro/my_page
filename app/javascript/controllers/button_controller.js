89dd9ef382d54559bdaddb10f15cc9d553d06ac5
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-03-19 12:42:59 -0300

    refact: changing text url

--- app/javascript/controllers/button_controller.js
+++ app/javascript/controllers/button_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   copy(event) {
     event.preventDefault()
     const currentLocation = window.location.href.split('/').pop();
-    const pathClipboard = `http://127.0.0.1:3000/my_annotations/${currentLocation}`
+    const pathClipboard = `http://giovannydev/my_annotations/${currentLocation}`
     navigator.clipboard.writeText(pathClipboard)
 
     // const div = "<div id='copy_clipboard' class='copy_clipboard'>URL Copiada!</div>"
